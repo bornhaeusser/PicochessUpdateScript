b699239b70b80996294edd97311c1f5f356fffa9
bornhaeusser <db@consolenrutscher.de>
2019-06-21 01:10:36 +0200

    Update picochess_201_newEngine.sh

--- picochess_201_newEngine.sh
+++ picochess_201_newEngine.sh
@@ -3,6 +3,8 @@
 #copy script into /home/pi/
 #Make Script Executable
 #sudo chmod +x picochess_201_newEngine.sh
+sudo apt-get update
+sudo apt-get upgrade
 
 sudo killall  python3
 
@@ -11,15 +13,17 @@ git clone https://github.com/tosca07/picochess.git
 git clone https://github.com/well69/Chess-Engines-for-Raspberry-Pi-by-Al.git
 
 #Safety Copy Picochess
-sudo cp -R /opt/picochess /opt/picochess_org
+sudo mkdir /opt/backup/
+sudo mkdir /opt/backup/picochess
+sudo cp -R /opt/picochess /opt/backup/picochess/picochess_$(date +%Y%m%d_%H%M%S)
 
 # Copy new Picochess 2.01
 sudo cp -R /home/pi/picochess/ /opt/
 
 #Install new Engines
 sudo apt install cmake g++ git libboost-all-dev libopenblas-dev opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev zlib1g-dev
-sudo mkdir /opt/picochess/engines/armv6l
-sudo mkdir /opt/picochess/engines/armv6l
+#sudo mkdir /opt/picochess/engines/armv6l
+#sudo mkdir /opt/picochess/engines/armv6l
 sudo cp -R Chess-Engines-for-Raspberry-Pi-by-Al/arm6l/* /opt/picochess/engines/armv6l
 sudo cp -R Chess-Engines-for-Raspberry-Pi-by-Al/arm7l/* /opt/picochess/engines/armv7l
 sudo chmod 755 -R /opt/picochess/engines
@@ -30,6 +34,10 @@ cp get_network\?sha\=20f06b2ee03c4a10a04202277facccdbdc9362b3360ac8c575c4e44c5ab
 rm get_network\?sha\=20f06b2ee03c4a10a04202277facccdbdc9362b3360ac8c575c4e44c5ab41ba7
 sudo mv weights.txt /opt/picochess/weigths.txt
 sudo chmod 755 /opt/picochess/weigths.txt
+
+#Cleaning up
+sudo rm -R Chess-Engines-for-Raspberry-Pi-by-Al
+sudo rm -R picochess
 
 #Clear History of you shell If you want
 #history -c
