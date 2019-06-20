9d2bb0516f256f98b0141ef96b5d52acb732c5c1
bornhaeusser <db@consolenrutscher.de>
2019-06-21 00:05:27 +0200

    Update picochess_201_newEngine.sh

--- picochess_201_newEngine.sh
+++ picochess_201_newEngine.sh
@@ -18,7 +18,10 @@ sudo cp -R /home/pi/picochess/ /opt/
 
 #Install new Engines
 sudo apt install cmake g++ git libboost-all-dev libopenblas-dev opencl-headers ocl-icd-libopencl1 ocl-icd-opencl-dev zlib1g-dev
-sudo cp -R Chess-Engines-for-Raspberry-Pi-by-Al/* /opt/picochess/engines
+sudo mkdir /opt/picochess/engines/armv6l
+sudo mkdir /opt/picochess/engines/armv6l
+sudo cp -R Chess-Engines-for-Raspberry-Pi-by-Al/arm6l/* /opt/picochess/engines/armv6l
+sudo cp -R Chess-Engines-for-Raspberry-Pi-by-Al/arm7l/* /opt/picochess/engines/armv7l
 sudo chmod 755 -R /opt/picochess/engines
 
 #Get Weights on http://lczero.org/networks/
